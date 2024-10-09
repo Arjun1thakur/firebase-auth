e9e5690924a2804dede6e4af03a220c0e90975de
Arjun1thakur <arjunthakur89096@gmail.com>
2024-10-09 17:13:55 +0530

    add

--- components/Registration/Registration.jsx
+++ components/Registration/Registration.jsx
@@ -13,6 +13,8 @@ import { setDoc, doc, getDoc } from "firebase/firestore";
 import { toast } from "sonner";
 import handleGithubSignIn from "@/providers/github/Github";
 import handleTwitterSignIn from "@/providers/twitter/Twitter";
+import handleMicrosoftSignIn from "@/providers/microsoft/Microsoft"
+import handleYahooSignIn from "@/providers/yahoo/Yahoo"
 import { getFacebookLoginStatus,initFacebookSdk,fbLogin } from "@/config/facebooksdk";
 
 const Registration = () => {
@@ -45,19 +47,7 @@ const Registration = () => {
     }
   };
 
-  const MicrosoftAuth =  async () => {
-      const provider = new OAuthProvider('microsoft.com');
-      
-      try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-        console.log('User:', result);
-  
-        // You can save the user info to your Firestore or Realtime Database if needed
-      } catch (error) {
-        console.error('Error during sign-in:', error);
-      }
-    };
+
 
   const requestOtp = (e) => {
     e.preventDefault();
@@ -232,6 +222,15 @@ const Registration = () => {
                   className="inline shrink-0" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
               </button>
               <button
+                onClick={handleYahooSignIn}
+                type="button"
+                className="p-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-[#816bff] hover:bg-[#412BBD]">
+                  <svg  width="22px"
+                  fill="#fff"
+                  className="inline shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M223.7 141.1 167 284.2 111 141.1H14.9L120.8 390.2 82.2 480h94.2L317.3 141.1zm105.4 135.8a58.2 58.2 0 1 0 58.2 58.2A58.2 58.2 0 0 0 329.1 276.9zM394.7 32l-93 223.5H406.4L499.1 32z"/></svg>
+              
+              </button>
+              <button
                 onClick={handleGithubSignIn}
                 type="button"
                 className="p-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-black hover:bg-[#333]">
@@ -247,24 +246,30 @@ const Registration = () => {
                   />
                 </svg>
               </button>
+              
+              <button
+                onClick={handleMicrosoftSignIn}
+                type="button"
+                className="p-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-gray-100 hover:bg-gray-200">
+             
+                  <svg  width="22px"
+                  fill="#fff"
+                  className="inline shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23"><path fill="#f3f3f3" d="M0 0h23v23H0z"/><path fill="#f35325" d="M1 1h10v10H1z"/><path fill="#81bc06" d="M12 1h10v10H12z"/><path fill="#05a6f0" d="M1 12h10v10H1z"/><path fill="#ffba08" d="M12 12h10v10H12z"/></svg>
+              
+              </button>
               <button
                 onClick={handleTwitterSignIn}
                 type="button"
-                className="p-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-black hover:bg-[#333]">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22px"
+                className="p-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-black hover:bg-[#333] ">
+                 
+                 <svg xmlns="http://www.w3.org/2000/svg" width="22px"
                   fill="#fff"
                   className="inline shrink-0" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
               
+                  
               </button>
-              <button
-                onClick={MicrosoftAuth}
-                type="button"
-                className="p-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-black hover:bg-[#333]">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22px"
-                  fill="#fff"
-                  className="inline shrink-0" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+             
               
-              </button>
             </div>
           </div>
 
